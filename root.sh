2733411c648956225b69b64b56c8b859096d8148
IzyanzZ <87628633+IzyanzZ@users.noreply.github.com>
2021-08-03 18:14:54 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,47 +1,6 @@
 # -----------
 # - IzyanzZ -
 # -----------
-
-  echo "I"
-  sleep 0.5
-  clear
-  echo "In"
-  sleep 0.5
-  clear
-  echo "Ins"
-  sleep 0.5
-  clear
-  echo "Inst"
-  sleep 0.5
-  clear
-  echo "Insta"
-  sleep 0.5
-  clear
-  echo "Instal"
-  sleep 0.5
-  clear
-  echo "Install"
-  sleep 0.5
-  clear
-  echo "Installi"
-  sleep 0.5
-  clear
-  echo "Installin"
-  sleep 0.5
-  clear
-  echo "Installing"
-  sleep 0.5
-  clear
-  echo "Installing ..."
-  sleep 0.5
-  clear
-  echo "Installing ."
-  sleep 0.5
-  clear
-  echo "Installing ..."
-  
-  
   pkg update -y
   pkg install curl proot tar -y
   curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
-  
